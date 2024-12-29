f9f138b2e77f923d2c67a7dd6647fc6e8a18435f
HeyJIOBUM <kirill.nichinger@gmail.com>
2024-12-29 12:25:03 +0300

    use MyUserService instead of MyUserDetailService

--- fintrack-backend/src/main/java/org/heyjiobum/fintrackbackend/security/jwt/JwtAuthenticationFilter.java
+++ fintrack-backend/src/main/java/org/heyjiobum/fintrackbackend/security/jwt/JwtAuthenticationFilter.java
@@ -5,32 +5,29 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.heyjiobum.fintrackbackend.security.model.MyUserDetailService;
+import org.heyjiobum.fintrackbackend.security.model.MyUserService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-import java.time.Instant;
 import java.util.Optional;
 import java.util.stream.Stream;
 
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
-    private final MyUserDetailService userDetailService;
+    private final MyUserService userService;
     private final String cookieName;
 
     public JwtAuthenticationFilter(JwtService jwtService,
-                                   MyUserDetailService userDetailService,
+                                   MyUserService userService,
                                    @Value("${jwt.cookie-name}") String cookieName) {
         this.jwtService = jwtService;
-        this.userDetailService = userDetailService;
+        this.userService = userService;
         this.cookieName = cookieName;
     }
 
@@ -38,31 +35,32 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
-        Optional<Cookie> cookieOptional = Stream.of(request.getCookies())
-                .filter(cookie -> cookie.getName().equals(cookieName))
-                .findFirst();
+        if (request.getCookies() != null){
+            Optional<Cookie> cookieOptional = Stream.of(request.getCookies())
+                    .filter(cookie -> cookie.getName().equals(cookieName))
+                    .findFirst();
 
-        if (cookieOptional.isPresent()) {
-            Cookie cookie = cookieOptional.get();
-            String token = cookie.getValue();
-
-            Instant expiration = jwtService.extractExpiration(token);
-
-            String username = jwtService.extractUsername(token);
-            try {
-                UserDetails userDetails = userDetailService.loadUserByUsername(username);
-                if (expiration.isAfter(Instant.now())) {
-                    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
-                            username,
-                            userDetails.getPassword(),
-                            userDetails.getAuthorities()
-                    );
-                    authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            if (cookieOptional.isPresent()) {
+                Cookie cookie = cookieOptional.get();
+                String token = cookie.getValue();
+                try {
+                    if (jwtService.isTokenValid(token)) {
+                        String username = jwtService.extractUsername(token);
+                        Optional<UserDetails> userDetailsOptional = userService.loadUserDetailsByUsername(username);
+                        if (userDetailsOptional.isPresent()) {
+                            UserDetails userDetails = userDetailsOptional.get();
+                            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                                    userDetails.getUsername(),
+                                    userDetails.getPassword(),
+                                    userDetails.getAuthorities()
+                            );
+                            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+                        }
+                    }
                 }
-            }
-            catch (UsernameNotFoundException e) {
-                filterChain.doFilter(request, response);
+                catch (Exception ignored) {
+                    filterChain.doFilter(request, response);
+                }
             }
         }
 
