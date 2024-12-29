eb5bf4b137bfc682981e0ec6beea27cc1542bc37
HeyJIOBUM <kirill.nichinger@gmail.com>
2024-12-29 12:27:03 +0300

    delete formLogin

--- fintrack-backend/src/main/java/org/heyjiobum/fintrackbackend/security/SecurityConfiguration.java
+++ fintrack-backend/src/main/java/org/heyjiobum/fintrackbackend/security/SecurityConfiguration.java
@@ -2,37 +2,27 @@ package org.heyjiobum.fintrackbackend.security;
 
 import lombok.AllArgsConstructor;
 import org.heyjiobum.fintrackbackend.security.jwt.JwtAuthenticationFilter;
-import org.heyjiobum.fintrackbackend.security.model.MyUserDetailService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.ProviderManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.csrf.CsrfFilter;
 
 @AllArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
-    private MyUserDetailService userDetailService;
     private SessionAuthenticationStrategy tokenCookieSessionAuthenticationStrategy;
     private JwtAuthenticationFilter jwtAuthenticationFilter;
-    private PasswordEncoder passwordEncoder;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
                 .authorizeHttpRequests(registry -> registry
                         .requestMatchers("/home", "/register", "/authenticate").permitAll()
                         .requestMatchers("/admin").hasRole("ADMIN")
@@ -41,20 +31,7 @@ public class SecurityConfiguration {
                 .sessionManagement(sessionManagement -> sessionManagement
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                         .sessionAuthenticationStrategy(tokenCookieSessionAuthenticationStrategy))
-                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(jwtAuthenticationFilter, CsrfFilter.class)
                 .build();
-    }
-
-    @Bean
-    public AuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setUserDetailsService(userDetailService);
-        provider.setPasswordEncoder(passwordEncoder);
-        return provider;
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager() {
-        return new ProviderManager(authenticationProvider());
     }
 }
