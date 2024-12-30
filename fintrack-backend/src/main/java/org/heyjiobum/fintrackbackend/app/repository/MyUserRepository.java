23611a9a854816a4cf58658ec0e21e28883450b9
HeyJIOBUM <kirill.nichinger@gmail.com>
2024-12-30 12:19:33 +0300

    add entity search

--- fintrack-backend/src/main/java/org/heyjiobum/fintrackbackend/app/repository/MyUserRepository.java
+++ fintrack-backend/src/main/java/org/heyjiobum/fintrackbackend/app/repository/MyUserRepository.java
@@ -1,10 +1,25 @@
 package org.heyjiobum.fintrackbackend.app.repository;
 
-import org.heyjiobum.fintrackbackend.app.entity.MyUser;
+import org.heyjiobum.fintrackbackend.app.entity.*;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MyUserRepository extends JpaRepository<MyUser, Long> {
     Optional<MyUser> findByUsername(String username);
+
+    @Query("select u.categories from MyUser u where u.username = :username")
+    List<Category> findCategoriesByUsername(@Param("username") String username);
+
+    @Query("select u.expenses from MyUser u where u.username = :username")
+    List<Expense> findExpenseByUsername(@Param("username") String username);
+
+    @Query("select u.budgets from MyUser u where u.username = :username")
+    List<Budget> findBudgetsByUsername(@Param("username") String username);
+
+    @Query("select u.incomes from MyUser u where u.username = :username")
+    List<Income> findIncomesByUsername(@Param("username") String username);
 }
