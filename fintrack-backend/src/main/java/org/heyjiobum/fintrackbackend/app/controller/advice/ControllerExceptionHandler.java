84b03655c1e9621cefff5639cdc2b718b15c7b40
HeyJIOBUM <kirill.nichinger@gmail.com>
2024-12-30 17:45:26 +0300

    handle HttpMessageNotReadableException and NoResourceFoundException

--- fintrack-backend/src/main/java/org/heyjiobum/fintrackbackend/app/controller/advice/ControllerExceptionHandler.java
+++ fintrack-backend/src/main/java/org/heyjiobum/fintrackbackend/app/controller/advice/ControllerExceptionHandler.java
@@ -1,36 +1,31 @@
-package org.heyjiobum.fintrackbackend.app.controller;
+package org.heyjiobum.fintrackbackend.app.controller.advice;
 
 import org.heyjiobum.fintrackbackend.app.error.ApplicationError;
-import org.springframework.context.MessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
-import org.springframework.validation.ObjectError;
-import org.springframework.validation.method.ParameterErrors;
-import org.springframework.validation.method.ParameterValidationResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
-import java.util.*;
+import java.util.List;
+import java.util.Objects;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
     @ExceptionHandler
     @ResponseBody
-    public ResponseEntity<?> catchMethodValidationException(HandlerMethodValidationException e) {
+    public ResponseEntity<?> catchMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         List<String> errors = e
                 .getAllErrors()
-                .stream()
+                .stream().filter(Objects::nonNull)
                 .map(messageSourceResolvable -> {
                     String parameterValue;
-                    if (messageSourceResolvable instanceof ObjectError objectError) {
-                        parameterValue = objectError.getObjectName();
-                    } else {
-                        parameterValue = ((MessageSourceResolvable) Objects.requireNonNull(messageSourceResolvable.getArguments())[0]).getDefaultMessage();
-                    }
+                    parameterValue = messageSourceResolvable.getObjectName();
 
                     String parameter = "In parameter " + parameterValue;
                     String error = messageSourceResolvable.getDefaultMessage();
@@ -38,14 +33,14 @@ public class ControllerExceptionHandler {
                     return parameter + ": " + error;
                 }).toList();
         return new ResponseEntity<>(
-                new ApplicationError(HttpStatus.NOT_FOUND.value(), String.join("\n", errors)),
+                new ApplicationError(HttpStatus.BAD_REQUEST.value(), String.join("\n", errors)),
                 HttpStatus.BAD_REQUEST
         );
     }
 
     @ExceptionHandler
     @ResponseBody
-    public ResponseEntity<?> catchMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ResponseEntity<?> catchMethodValidationException(HandlerMethodValidationException e) {
         List<String> errors = e
                 .getAllErrors()
                 .stream()
@@ -57,8 +52,26 @@ public class ControllerExceptionHandler {
                 }).toList();
 
         return new ResponseEntity<>(
-                new ApplicationError(HttpStatus.NOT_FOUND.value(), String.join("\n", errors)),
+                new ApplicationError(HttpStatus.BAD_REQUEST.value(), String.join("\n", errors)),
                 HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseBody
+    public ResponseEntity<?> catchHttpMessageNotReadableException(HttpMessageNotReadableException e) {
+        return new ResponseEntity<>(
+                new ApplicationError(HttpStatus.BAD_REQUEST.value(), e.getMessage()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseBody
+    public ResponseEntity<?> catchNoResourceFoundException(NoResourceFoundException e) {
+        return new ResponseEntity<>(
+                new ApplicationError(HttpStatus.NOT_FOUND.value(),e.getMessage()),
+                HttpStatus.NOT_FOUND
         );
     }
 }
