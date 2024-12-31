f25e44285a2c24414aabcbda0fd95347711a1968
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-01-01 01:21:24 +0300

    rewrite AuthModal

--- fintrack-frontend/src/components/auth-modal.js
+++ fintrack-frontend/src/components/auth-modal.js
@@ -1,69 +1,77 @@
 'use client';
 
-import {useState} from 'react';
 import {CircleX} from 'lucide-react';
-import {logIn, logOut} from "@/redux/slices/auth-slice"
-import {useDispatch, useSelector} from "react-redux";
-import {deleteTokenCookie} from "@/app/actions";
 
-const AuthModal = () => {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+import {useState} from 'react';
+import {useDispatch} from "react-redux";
+
+import {logIn} from "@/redux/slices/auth-slice"
+import {register, signIn} from "@/configuration/api/auth-api";
+
+const AuthModal = ({isOpen, onClose, onAuthentication}) => {
+    const [errorMessage, setErrorMessage] = useState(null);
+
     const [isSignInForm, setIsSignInForm] = useState(true);
 
-    const dispatch = useDispatch();
-    const userData = useSelector((state) => state.authReducer.value);
-
-    const handleModalOpenToggle = () => {
-        setIsModalOpen(!isModalOpen)
-    }
+    const dispatch = useDispatch()
 
     const handleModalTypeToggle = () => {
         setIsSignInForm(!isSignInForm);
+        setErrorMessage(null);
     };
 
-    const handleSubmit = (e) => {
+    const handleModalClose = () => {
+        setErrorMessage(null);
+        onClose();
+    }
+
+    const handleAuthentication = () => {
+        setErrorMessage(null);
+        onAuthentication();
+    }
+
+    const handleSubmit = async (e) => {
         e.preventDefault();
         const formData = new FormData(e.currentTarget);
+
         const username = formData.get('username');
         const password = formData.get('password');
         const confirmPassword = formData.get('confirmPassword');
-        console.log('Form submitted:', { username: username, password: password, confirmPassword: confirmPassword });
 
-        dispatch(logIn(username));
-        setIsModalOpen(false);
+        if (!isSignInForm && confirmPassword !== password) {
+            setErrorMessage("Passwords must be the same");
+            return
+        }
+
+        const authFunction = isSignInForm ? signIn : register;
+
+        const {statusCode, message} = await authFunction({username, password});
+        if (statusCode === 200){
+            dispatch(logIn(username));
+            setErrorMessage(null);
+            handleAuthentication();
+        }
+        else {
+            setErrorMessage(message);
+        }
     };
 
-    const handleLogOut = (e) => {
-        deleteTokenCookie().then(r => dispatch(logOut()));
+    const clearErrorMessage = () => {
+        setErrorMessage(null);
     }
 
     return (
         <>
             {
-                userData.isAuthenticated ? (
-                    <button
-                        className="text-lg font-semibold"
-                        onClick={handleLogOut}>
-                        Log out
-                    </button>
-                ) : (
-                    <button
-                        className="text-lg font-semibold"
-                        onClick={handleModalOpenToggle}>
-                        Sign in
-                    </button>
-                )
-            }
-            {
-                isModalOpen && (
-                    <div className="fixed inset-0 flex items-center justify-center bg-black/50">
-                        <div className="relative rounded bg-white p-6 pt-3 shadow-md">
+                isOpen && (
+                    <div className="fixed inset-0 z-10 flex items-center justify-center bg-black/50">
+                        <div className="relative max-w-screen-sm rounded bg-white p-6 pt-3 shadow-md">
                             <div className="grow pb-1 text-center text-xl">
                                 {isSignInForm ? 'Sign in form' : 'Register form'}
                             </div>
                             {/*CloseButton*/}
                             <button
-                                onClick={handleModalOpenToggle}
+                                onClick={handleModalClose}
                                 aria-label="Close"
                                 className="absolute right-2 top-[13px]"
                             >
@@ -79,30 +87,39 @@ const AuthModal = () => {
                                     <input type="text"
                                            name="username"
                                            className="w-full rounded border px-3 py-2 text-black"
+                                           onChange={clearErrorMessage}
                                            required/>
                                 </div>
-                                <div className="mb-4">
+                                <div>
                                     <label className="mb-1 block text-black">
                                         Password
                                     </label>
                                     <input type="password"
                                            name="password"
                                            className="w-full rounded border px-3 py-2 text-black"
+                                           onChange={clearErrorMessage}
                                            required/>
                                 </div>
                                 {!isSignInForm && (
-                                    <div className="mb-4">
+                                    <div className="mt-4">
                                         <label className="mb-1 block text-black">
                                             Confirm password
                                         </label>
                                         <input type="password"
                                                name="confirmPassword"
                                                className="w-full rounded border px-3 py-2 text-black"
+                                               onChange={clearErrorMessage}
                                                required/>
                                     </div>
                                 )}
+                                {/*Error message*/}
+                                {errorMessage !== null && (
+                                    <p className="mb-[-12px] mt-1 text-center text-xs text-red-600">
+                                        {errorMessage}
+                                    </p>
+                                )}
                                 <button type="submit"
-                                        className="rounded bg-black px-4 py-2 text-white transition duration-200 hover:bg-gray-800">
+                                        className="mt-4 rounded bg-black px-4 py-2 text-white transition duration-200 hover:bg-gray-800">
                                     Submit
                                 </button>
                             </form>
--- fintrack-frontend/src/components/header.js
+++ fintrack-frontend/src/components/header.js
@@ -4,7 +4,25 @@ import Image from 'next/image'
 import Link from 'next/link';
 import AuthModal from "@/components/auth-modal";
 
+import {deleteTokenCookie} from "@/app/actions";
+import {useDispatch, useSelector} from "react-redux";
+import {logOut} from "@/redux/slices/auth-slice"
+import {useState} from "react";
+
 export default function Header() {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const dispatch = useDispatch();
+    const userData = useSelector((state) => state.authReducer.value);
+
+    const handleModalOpenToggle = () => {
+        setIsModalOpen(!isModalOpen);
+    }
+
+    const handleLogOut = () => {
+        deleteTokenCookie().then(() => dispatch(logOut()));
+    }
+
     return (
         <header className="sticky top-0 z-[1] flex h-16 items-center justify-between border-b bg-background px-8">
             <Link
@@ -15,7 +33,23 @@ export default function Header() {
                 <span>HeyJIOBUM FinancialTracking</span>
             </Link>
 
-            <AuthModal/>
+            {
+                userData.isAuthenticated ? (
+                    <button
+                        className="text-lg font-semibold"
+                        onClick={handleLogOut}>
+                        Log out
+                    </button>
+                ) : (
+                    <button
+                        className="text-lg font-semibold"
+                        onClick={handleModalOpenToggle}>
+                        Sign in
+                    </button>
+                )
+            }
+
+            <AuthModal isOpen={isModalOpen} onClose={handleModalOpenToggle} onAuthentication={handleModalOpenToggle}/>
         </header>
     );
 }
