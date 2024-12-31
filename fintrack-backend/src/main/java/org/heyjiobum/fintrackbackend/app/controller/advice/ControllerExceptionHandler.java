a81d745c206543f583b42bb51896e69d82088910
HeyJIOBUM <kirill.nichinger@gmail.com>
2024-12-31 19:10:01 +0300

    handle BadCredentialsException

--- fintrack-backend/src/main/java/org/heyjiobum/fintrackbackend/app/controller/advice/ControllerExceptionHandler.java
+++ fintrack-backend/src/main/java/org/heyjiobum/fintrackbackend/app/controller/advice/ControllerExceptionHandler.java
@@ -4,6 +4,7 @@ import org.heyjiobum.fintrackbackend.app.error.ApplicationError;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -74,4 +75,14 @@ public class ControllerExceptionHandler {
                 HttpStatus.NOT_FOUND
         );
     }
+
+    @ExceptionHandler
+    @ResponseBody
+    public ResponseEntity<?> catchBadCredentialsException(BadCredentialsException e) {
+        return new ResponseEntity<>(
+                new ApplicationError(HttpStatus.BAD_REQUEST.value(),e.getMessage()),
+                HttpStatus.BAD_REQUEST
+        );
+    }
+
 }
