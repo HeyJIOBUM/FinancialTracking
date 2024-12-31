e6a11601546a747c6b556722ce5c768b805844d8
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-01-01 01:19:27 +0300

    change function return

--- fintrack-frontend/src/configuration/api/auth-api.js
+++ fintrack-frontend/src/configuration/api/auth-api.js
@@ -1,20 +1,28 @@
 import {base_api_url} from "@/configuration/api/application-api";
 
 async function authenticateUser(endpoint, username, password) {
+    let response = null;
     try {
-        const response = await fetch(base_api_url + endpoint, {
+        response = await fetch(base_api_url + endpoint, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
             credentials: 'include',
-            body: JSON.stringify({ username, password }),
+            body: JSON.stringify({username, password}),
         });
-
-        return {ok: response.ok, errorMsg: response.ok ? undefined : response.statusText};
     }
     catch (err) {
-        return {ok: false, errorMsg: `Error: ${err.message}`};
+        return {statusCode: 500, message: `Server error: ${err.message}`};
+    }
+
+    try {
+        const data = await response.json();
+        return {statusCode: data.statusCode, message: data.message};
+    }
+    catch (err) {
+        // server doesn't return data so response.json() may throw an exception
+        return {statusCode: 200, message: `Response body is empty`};
     }
 }
 
