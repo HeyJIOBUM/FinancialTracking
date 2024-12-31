92976652c9a9d02badbc20dc74689196bab8c711
HeyJIOBUM <kirill.nichinger@gmail.com>
2024-12-31 20:51:03 +0300

    handle additional exceptions

--- fintrack-backend/src/main/java/org/heyjiobum/fintrackbackend/app/controller/advice/ControllerExceptionHandler.java
+++ fintrack-backend/src/main/java/org/heyjiobum/fintrackbackend/app/controller/advice/ControllerExceptionHandler.java
@@ -6,11 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.annotation.HandlerMethodValidationException;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 import java.util.List;
@@ -33,6 +36,7 @@ public class ControllerExceptionHandler {
 
                     return parameter + ": " + error;
                 }).toList();
+
         return new ResponseEntity<>(
                 new ApplicationError(HttpStatus.BAD_REQUEST.value(), String.join("\n", errors)),
                 HttpStatus.BAD_REQUEST
@@ -71,7 +75,7 @@ public class ControllerExceptionHandler {
     @ResponseBody
     public ResponseEntity<?> catchNoResourceFoundException(NoResourceFoundException e) {
         return new ResponseEntity<>(
-                new ApplicationError(HttpStatus.NOT_FOUND.value(),e.getMessage()),
+                new ApplicationError(HttpStatus.NOT_FOUND.value(), e.getMessage()),
                 HttpStatus.NOT_FOUND
         );
     }
@@ -80,9 +84,55 @@ public class ControllerExceptionHandler {
     @ResponseBody
     public ResponseEntity<?> catchBadCredentialsException(BadCredentialsException e) {
         return new ResponseEntity<>(
-                new ApplicationError(HttpStatus.BAD_REQUEST.value(),e.getMessage()),
+                new ApplicationError(HttpStatus.BAD_REQUEST.value(), e.getMessage()),
                 HttpStatus.BAD_REQUEST
         );
     }
 
+    @ExceptionHandler
+    @ResponseBody
+    public ResponseEntity<?> catchHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        StringBuilder messageBuilder = new StringBuilder();
+        messageBuilder.append(e.getMethod());
+        messageBuilder.append(" method is not supported for this request. Supported methods are ");
+        Objects.requireNonNull(e.getSupportedHttpMethods()).forEach(t -> messageBuilder.append(t).append(" "));
+
+        return new ResponseEntity<>(
+                new ApplicationError(HttpStatus.NOT_FOUND.value(), messageBuilder.toString()),
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseBody
+    public ResponseEntity<?> catchNoHandlerFoundException(NoHandlerFoundException e) {
+        String message = "No handler found for " + e.getHttpMethod() + " " + e.getRequestURL();
+
+        return new ResponseEntity<>(
+                new ApplicationError(HttpStatus.NOT_FOUND.value(), message),
+                HttpStatus.NOT_FOUND
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseBody
+    public ResponseEntity<?> catchHttpMediaTypeNotSupportedException(HttpMediaTypeNotSupportedException e) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(e.getContentType());
+        builder.append(" media type is not supported. Supported media types are ");
+        e.getSupportedMediaTypes().forEach(t -> builder.append(t).append(", "));
+
+        return new ResponseEntity<>(
+                new ApplicationError(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value(), builder.toString()),
+                HttpStatus.UNSUPPORTED_MEDIA_TYPE
+        );
+    }
+
+    @ExceptionHandler({ Exception.class })
+    public ResponseEntity<Object> catchAll(Exception e) {
+        return new ResponseEntity<>(
+                new ApplicationError(HttpStatus.INTERNAL_SERVER_ERROR.value(), e.getMessage()),
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
 }
