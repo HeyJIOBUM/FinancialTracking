053d230201a4fa1e374ff85146987ae50810c1b5
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-01-01 14:39:03 +0300

    format @Pattern message

--- fintrack-backend/src/main/java/org/heyjiobum/fintrackbackend/app/entity/dto/MyUserDTO.java
+++ fintrack-backend/src/main/java/org/heyjiobum/fintrackbackend/app/entity/dto/MyUserDTO.java
@@ -6,10 +6,12 @@ import org.heyjiobum.fintrackbackend.app.entity.MyUser;
 
 @AllArgsConstructor
 public class MyUserDTO {
-    @Pattern(regexp = "^[a-zA-Z0-9]{4,20}$", message = "Username must have from 4 to 20 characters, only numbers and letters are allowed")
+    @Pattern(regexp = "^[a-zA-Z0-9]{4,20}$",
+            message = "Username must have from 4 to 20 characters, only numbers and letters are allowed")
     private String username;
 
-    @Pattern(regexp = "^[a-zA-Z0-9]{4,20}$", message = "Password must have from 4 to 20 characters, only numbers and letters are allowed")
+    @Pattern(regexp = "^[a-zA-Z0-9]{4,20}$",
+            message = "Password must have from 4 to 20 characters, only numbers and letters are allowed")
     private String password;
 
     public MyUser constructMyUser() {
