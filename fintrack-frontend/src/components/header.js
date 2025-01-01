e15d288137a6895b3f9888b230b7e7f5730751a2
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-01-01 11:29:31 +0300

    reload page if log out

--- fintrack-frontend/src/components/header.js
+++ fintrack-frontend/src/components/header.js
@@ -20,7 +20,9 @@ export default function Header() {
     }
 
     const handleLogOut = () => {
-        deleteTokenCookie().then(() => dispatch(logOut()));
+        deleteTokenCookie();
+        dispatch(logOut());
+        window.location.reload();
     }
 
     return (
