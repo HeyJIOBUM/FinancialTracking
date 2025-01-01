630254d675cea08e9c3158713a9ea27aaf459eec
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-01-02 00:07:17 +0300

    disable horizontal scrolling

--- fintrack-frontend/src/components/loading.js
+++ fintrack-frontend/src/components/loading.js
@@ -1,7 +1,11 @@
 export default function Loading() {
     return (
-        <div>
-            Settings
-        </div>
+        <>
+            {/*Loading is very fast => now disabled*/}
+
+            {/*<Skeleton className="size-full rounded-lg">*/}
+                {/*<div className="h-[100px] rounded-xl bg-gray-100"/>*/}
+            {/*</Skeleton>*/}
+        </>
     );
 }
