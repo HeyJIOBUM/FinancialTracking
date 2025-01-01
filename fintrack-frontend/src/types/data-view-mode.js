f35578a37b3f4250fb7a6de137b16442a81dbd27
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-01-01 18:29:12 +0300

    add enums

---
+++ fintrack-frontend/src/types/data-view-mode.js
@@ -0,0 +1,4 @@
+export class DataViewMode {
+    static get GRAPHIC() { return "Graphic"; }
+    static get LIST() { return "List"; }
+}
---
+++ fintrack-frontend/src/types/operation-type.js
@@ -0,0 +1,4 @@
+export class OperationType {
+    static get INCOME() { return "Income"; }
+    static get EXPENSE() { return "Expense"; }
+}
