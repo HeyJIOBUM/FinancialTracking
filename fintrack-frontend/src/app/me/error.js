97bd05ac5b2ac631eb3d5d424499733ae13e1260
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-01-01 12:52:00 +0300

    add error page

---
+++ fintrack-frontend/src/app/me/error.js
@@ -0,0 +1,19 @@
+'use client'
+
+export default function Error({ error, reset }) {
+    const handleErrorReset = () => {
+        window.location.reload();
+    }
+
+    return (
+        <div className="flex flex-col items-center justify-center rounded-lg border border-red-500 bg-red-50 p-6 text-red-700">
+            <h2 className="mb-4 text-lg font-bold">Something went wrong!</h2>
+            <button
+                onClick={handleErrorReset}
+                className="rounded bg-red-500 px-4 py-2 text-white hover:bg-red-600"
+            >
+                Try again
+            </button>
+        </div>
+    );
+}
