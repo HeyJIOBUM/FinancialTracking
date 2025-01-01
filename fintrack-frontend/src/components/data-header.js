96fcabf90c84f24cde8fa5fa28c4339ab85d5a57
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-01-01 23:34:42 +0300

    refactor DataHeader props

--- fintrack-frontend/src/components/data-header.js
+++ fintrack-frontend/src/components/data-header.js
@@ -7,75 +7,87 @@ import {DataViewMode} from "@/types/data-view-mode";
 
 export default function DataHeader({
                                        categoriesOperationType,
-                                       chosenCategories,
-                                       setChosenCategories,
-                                       fromDate,
-                                       setFromDate,
-                                       toDate,
-                                       setToDate,
-                                       dataViewMode,
-                                       setDataViewMode
+                                       dataHeaderState,
+                                       setDataHeaderState
                                    }) {
-    const {data: categories, error: categoriesError, isLoading: isCategoriesLoading} = useGetCategoriesQuery();
+    const { chosenCategories, fromDate, toDate, dataViewMode } = dataHeaderState;
+
+    const { data: categories, error: categoriesError, isLoading: isCategoriesLoading } = useGetCategoriesQuery();
 
     if (categoriesError) {
         throw new Error(categoriesError.error);
     }
 
     if (isCategoriesLoading) {
-        return <Loading/>;
+        return <Loading />;
     }
 
     const availableCategories = categories.filter(category => category.operationType === categoriesOperationType);
 
     const handleFromDateChange = (e) => {
-        setFromDate(e.target.value);
+        setDataHeaderState(prevState => ({
+            ...prevState,
+            fromDate: e.target.value
+        }));
     };
 
     const handleToDateChange = (e) => {
-        setToDate(e.target.value);
+        setDataHeaderState(prevState => ({
+            ...prevState,
+            toDate: e.target.value
+        }));
     };
 
     const toggleDataViewMode = (mode) => {
-        setDataViewMode(mode);
+        setDataHeaderState(prevState => ({
+            ...prevState,
+            dataViewMode: mode
+        }));
     };
 
-    return (<div className="mb-4 flex items-center justify-between rounded-md border border-gray-300 px-8 py-4">
-        <div className="flex items-center space-x-4">
-            <button
-                className={`rounded border-b-4 px-4 py-2 ${dataViewMode === DataViewMode.GRAPHIC ? 'border-black' : 'border-transparent'} bg-white`}
-                onClick={() => toggleDataViewMode(DataViewMode.GRAPHIC)}
-            >
-                Graphic View
-            </button>
-            <button
-                className={`rounded border-b-4 px-4 py-2 ${dataViewMode === DataViewMode.LIST ? 'border-black' : 'border-transparent'} bg-white`}
-                onClick={() => toggleDataViewMode(DataViewMode.LIST)}
-            >
-                List View
-            </button>
-        </div>
+    return (
+        <div className="mb-4 flex items-center justify-between rounded-md border border-gray-300 px-8 py-4">
+            <div className="flex items-center space-x-4">
+                <button
+                    className={`rounded border-b-4 px-4 py-2 ${dataViewMode === DataViewMode.GRAPHIC ? 'border-black' : 'border-transparent'} bg-white`}
+                    onClick={() => toggleDataViewMode(DataViewMode.GRAPHIC)}
+                >
+                    Graphic View
+                </button>
+                <button
+                    className={`rounded border-b-4 px-4 py-2 ${dataViewMode === DataViewMode.LIST ? 'border-black' : 'border-transparent'} bg-white`}
+                    onClick={() => toggleDataViewMode(DataViewMode.LIST)}
+                >
+                    List View
+                </button>
+            </div>
 
+            <div className="flex items-center space-x-4">
+                <input
+                    type="date"
+                    value={fromDate}
+                    onChange={handleFromDateChange}
+                    className="rounded border border-gray-300 p-2"
+                />
+                <input
+                    type="date"
+                    value={toDate}
+                    onChange={handleToDateChange}
+                    className="rounded border border-gray-300 p-2"
+                />
+            </div>
 
-        <div className="flex items-center space-x-4">
-            <input
-                type="date"
-                value={fromDate}
-                onChange={handleFromDateChange}
-                className="rounded border border-gray-300 p-2"
+            <Select
+                options={availableCategories}
+                selectedOptions={chosenCategories}
+                setSelectedOptions={(selected) =>
+                    setDataHeaderState(prevState => ({
+                        ...prevState,
+                        chosenCategories: selected
+                    }))
+                }
+                isMultiSelect={true}
             />
-            <input
-                type="date"
-                value={toDate}
-                onChange={handleToDateChange}
-                className="rounded border border-gray-300 p-2"
-            />
         </div>
-
-        <Select
-            options={availableCategories}
-            selectedOptions={chosenCategories}
-            setSelectedOptions={setChosenCategories}
-            isMultiSelect={true}/>
-    </div>);
+    );
 }
