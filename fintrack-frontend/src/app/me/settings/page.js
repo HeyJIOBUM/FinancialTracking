89dc40cb26433b6e285711b6fbbb07955f3019a6
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-01-03 00:10:21 +0300

    change SettingsPage

--- fintrack-frontend/src/app/me/settings/page.js
+++ fintrack-frontend/src/app/me/settings/page.js
@@ -1,7 +1,9 @@
 export default function SettingsPage() {
     return (
-        <div>
-            Settings
+        <div className="bg-yellow-100 border-l-8 border-yellow-500 text-yellow-700 p-4">
+            This page is currently in development.
         </div>
+        // TODO:
+        // Add password change
     );
 }
