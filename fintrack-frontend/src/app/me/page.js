854f3cfc2695439b7db101c419d3b627af1d90fa
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-01-03 00:01:27 +0300

    add page with expenses and incomes graphic and update dependencies

--- fintrack-frontend/src/app/me/page.js
+++ fintrack-frontend/src/app/me/page.js
@@ -1,7 +1,73 @@
-export default function ProfilePage() {
+"use client";
+
+import {useGetExpensesQuery} from "@/configuration/api/expenses-api";
+import {useGetIncomesQuery} from "@/configuration/api/incomes-api";
+import React, {useState} from "react";
+import {formatDateToISO, isDateInRange, subtractMonthsFromDate} from "@/utils/date-utils";
+import Loading from "@/components/loading";
+import MoneyFlowSankeyChart from "@/components/graphic/money-flow-sankey-chart";
+
+export default function MoneyFlowPage() {
+    const {data: expenses, error: expensesError, isLoading: isExpensesLoading} = useGetExpensesQuery();
+    const {data: incomes, error: incomesError, isLoading: isIncomesLoading} = useGetIncomesQuery();
+
+    const dataRangeInitialState = {
+        fromDate: formatDateToISO(subtractMonthsFromDate(new Date(), 1)),
+        toDate: formatDateToISO(new Date()),
+    };
+
+    const [dataRange, setDataRange] = useState(dataRangeInitialState);
+
+    const filterOperations = (operations) => {
+        return operations.filter(operation =>
+            isDateInRange(operation.date, dataRange.fromDate, dataRange.toDate)
+        );
+    };
+    const handleDateChange = (e) => {
+        const {name, value} = e.target;
+        setDataRange(prevData => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+    if (isExpensesLoading || isIncomesLoading) {
+        return <Loading />;
+    }
+
+    if (incomesError) {
+        throw new Error(incomesError.error);
+    }
+
+    if (expensesError) {
+        throw new Error(expensesError.error);
+    }
+
+    const filteredExpenses = filterOperations(expenses);
+    const filteredIncomes = filterOperations(incomes);
+
     return (
-        <div>
-            Profile
-        </div>
+        <>
+            <div className="mb-4 flex justify-center rounded-md border border-gray-300 px-8 py-4">
+                <div className="flex space-x-4">
+                    <input
+                        type="date"
+                        value={dataRange.fromDate}
+                        onChange={handleDateChange}
+                        className="rounded border border-gray-300 p-2"
+                    />
+                    <input
+                        type="date"
+                        value={dataRange.toDate}
+                        onChange={handleDateChange}
+                        className="rounded border border-gray-300 p-2"
+                    />
+                </div>
+            </div>
+            <MoneyFlowSankeyChart
+                expenses={filteredExpenses}
+                incomes={filteredIncomes}
+            />
+        </>
     );
 }
---
+++ fintrack-frontend/src/components/graphic/money-flow-sankey-chart.js
@@ -0,0 +1,86 @@
+"use client";
+
+import React from "react";
+import {Chart} from "react-google-charts";
+import {OperationType} from "@/types/operation-type";
+
+export default function MoneyFlowSankeyChart({ expenses, incomes }) {
+    const flattenMoneyOperations = (moneyOperations) => {
+        return moneyOperations.reduce((acc, op) => {
+            const existingCategory = acc.find(item => item.id === op.category.id);
+            if (existingCategory) {
+                existingCategory.amount += op.amount;
+                existingCategory.tooltip = `${op.category.operationType}: ${op.category.name}, ${existingCategory.amount} Br`
+            } else {
+                acc.push({
+                    id: op.category.id,
+                    from: op.category.name,
+                    to: op.category.operationType,
+                    amount: op.amount,
+                    tooltip: `${op.category.operationType}: ${op.category.name}, ${op.amount} Br`,
+                });
+            }
+            return acc;
+        }, []);
+    }
+
+    if (expenses.length === 0 && incomes.length === 0) {
+        return (
+            <div className="flex flex-col gap-4 rounded border border-gray-300 p-4">
+                <p>No expenses and incomes available.</p>
+            </div>
+        );
+    }
+
+    const totalExpensesAmount = expenses.reduce((acc, op) => acc + op.amount, 0);
+    const totalIncomesAmount = incomes.reduce((acc, op) => acc + op.amount, 0);
+
+    const flattenedExpenses = flattenMoneyOperations(expenses);
+    const flattenedIncomes = flattenMoneyOperations(incomes);
+
+    console.log(flattenedExpenses);
+
+    const prepareSankeysData = () => {
+        const sankeysData = [["From", "To", "Weight", {type: 'string', role: 'tooltip'}]]
+
+        flattenedExpenses.forEach(expense => {
+            sankeysData.push([expense.from, expense.to, expense.amount, expense.tooltip]);
+        });
+
+        flattenedIncomes.forEach(income => {
+            sankeysData.push([income.from, income.to, income.amount, income.tooltip]);
+        });
+
+        return sankeysData;
+    };
+
+
+    const sankeysData = prepareSankeysData();
+
+    sankeysData.push(
+        [OperationType.EXPENSE, "Result", totalExpensesAmount, `Overall expense: ${totalExpensesAmount} Br`],
+        [OperationType.INCOME, "Result", totalIncomesAmount, `Overall income: ${totalIncomesAmount} Br`],
+    )
+
+    const options = {
+        sankey: {
+            node: {
+                label: {
+                    fontSize: 16,
+                },
+                nodePadding: 20
+            },
+        },
+    };
+
+    return (
+        <div className="rounded border border-gray-300 p-4">
+            <Chart
+                chartType="Sankey"
+                data={sankeysData}
+                height={400}
+                options={options}
+            />
+        </div>
+    );
+}
