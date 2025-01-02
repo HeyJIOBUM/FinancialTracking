43b45792a7ab06e98463ae6422a648f32adecd36
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-01-02 21:42:25 +0300

    change home page

--- fintrack-frontend/src/app/page.js
+++ fintrack-frontend/src/app/page.js
@@ -1,75 +1,14 @@
 export default function Home() {
     return (
-        <div>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum est dolor, nec pharetra mi feugiat non.
-            Etiam suscipit nisi augue, sit amet fringilla risus ornare id. Donec mi risus, tristique vitae felis non,
-            dapibus efficitur nunc. Fusce scelerisque lacus a neque fringilla auctor. Sed ullamcorper nulla nec lorem
-            placerat interdum. Vestibulum magna diam, sodales non luctus accumsan, iaculis vitae elit. Integer tempus
-            posuere elit eget facilisis. Maecenas convallis aliquam viverra. Pellentesque imperdiet elementum lorem, id
-            venenatis est vehicula at. Aenean in tortor imperdiet, rutrum nunc ac, sollicitudin lectus. Cras finibus
-            tempus magna eget egestas. Nam libero metus, tincidunt eu sagittis quis, vehicula ut orci. Fusce accumsan
-            lorem eros, non tincidunt quam fermentum nec.
-
-            Suspendisse aliquet est arcu, sit amet consectetur ante hendrerit ut. Praesent vulputate egestas interdum.
-            Nunc feugiat lacus odio, mollis sodales ipsum egestas eu. Donec nec libero porttitor, pharetra lacus sed,
-            congue ipsum. Ut condimentum gravida sapien quis vehicula. Maecenas volutpat non elit scelerisque molestie.
-            Maecenas fermentum odio enim, at lacinia neque placerat at.
-
-            Suspendisse sodales dui vestibulum, sodales nisl vitae, fringilla nisl. Etiam luctus urna magna, nec
-            ullamcorper magna lobortis sit amet. Morbi accumsan risus eu fringilla viverra. Fusce lacinia hendrerit
-            hendrerit. Aliquam in pharetra odio. Donec ac pretium eros. Proin porta ligula id metus cursus ultricies.
-
-            Curabitur pulvinar molestie lacus sit amet commodo. Fusce iaculis ante ut nibh varius, ac sollicitudin
-            ligula lacinia. Aenean vel luctus mauris. Nunc lectus ex, luctus in hendrerit at, consequat et nunc. Fusce
-            fringilla ex placerat molestie vehicula. Cras luctus vestibulum nunc, et maximus purus suscipit vitae.
-            Phasellus scelerisque commodo libero. Donec urna felis, interdum sed condimentum in, dapibus ac sem. Nullam
-            tincidunt ex et aliquet consectetur. Maecenas malesuada nulla et ante placerat, eget tempus mauris interdum.
-            Nulla nec nisi est. Maecenas non nulla justo.
-
-            Sed velit ipsum, facilisis a augue vitae, tempus fringilla lectus. Morbi vel vulputate eros, a semper odio.
-            Phasellus elit quam, maximus sed ligula vel, egestas placerat dui. Ut sagittis, lectus non iaculis ornare,
-            ipsum ante aliquet diam, maximus maximus magna dolor ut sapien. Proin tincidunt accumsan iaculis. Phasellus
-            pretium, ex eu imperdiet placerat, urna turpis gravida quam, et sodales ligula mauris ac felis. Suspendisse
-            vel lacus eleifend, sollicitudin eros posuere, hendrerit quam. Vivamus sit amet elit vel lorem bibendum
-            molestie. Nullam eleifend congue nisl ac imperdiet. In vel nulla fermentum, dignissim ex in, congue dolor.
-            Maecenas quis malesuada nisl, id facilisis nisl. Pellentesque ante sem, molestie in velit et, porttitor
-            consequat tellus. Nunc blandit, turpis vitae auctor placerat, nulla nisl aliquam ante, a interdum mauris
-            velit in neque.
-
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed rutrum est dolor, nec pharetra mi feugiat non.
-            Etiam suscipit nisi augue, sit amet fringilla risus ornare id. Donec mi risus, tristique vitae felis non,
-            dapibus efficitur nunc. Fusce scelerisque lacus a neque fringilla auctor. Sed ullamcorper nulla nec lorem
-            placerat interdum. Vestibulum magna diam, sodales non luctus accumsan, iaculis vitae elit. Integer tempus
-            posuere elit eget facilisis. Maecenas convallis aliquam viverra. Pellentesque imperdiet elementum lorem, id
-            venenatis est vehicula at. Aenean in tortor imperdiet, rutrum nunc ac, sollicitudin lectus. Cras finibus
-            tempus magna eget egestas. Nam libero metus, tincidunt eu sagittis quis, vehicula ut orci. Fusce accumsan
-            lorem eros, non tincidunt quam fermentum nec.
-
-            Suspendisse aliquet est arcu, sit amet consectetur ante hendrerit ut. Praesent vulputate egestas interdum.
-            Nunc feugiat lacus odio, mollis sodales ipsum egestas eu. Donec nec libero porttitor, pharetra lacus sed,
-            congue ipsum. Ut condimentum gravida sapien quis vehicula. Maecenas volutpat non elit scelerisque molestie.
-            Maecenas fermentum odio enim, at lacinia neque placerat at.
-
-            Suspendisse sodales dui vestibulum, sodales nisl vitae, fringilla nisl. Etiam luctus urna magna, nec
-            ullamcorper magna lobortis sit amet. Morbi accumsan risus eu fringilla viverra. Fusce lacinia hendrerit
-            hendrerit. Aliquam in pharetra odio. Donec ac pretium eros. Proin porta ligula id metus cursus ultricies.
-
-            Curabitur pulvinar molestie lacus sit amet commodo. Fusce iaculis ante ut nibh varius, ac sollicitudin
-            ligula lacinia. Aenean vel luctus mauris. Nunc lectus ex, luctus in hendrerit at, consequat et nunc. Fusce
-            fringilla ex placerat molestie vehicula. Cras luctus vestibulum nunc, et maximus purus suscipit vitae.
-            Phasellus scelerisque commodo libero. Donec urna felis, interdum sed condimentum in, dapibus ac sem. Nullam
-            tincidunt ex et aliquet consectetur. Maecenas malesuada nulla et ante placerat, eget tempus mauris interdum.
-            Nulla nec nisi est. Maecenas non nulla justo.
-
-            Sed velit ipsum, facilisis a augue vitae, tempus fringilla lectus. Morbi vel vulputate eros, a semper odio.
-            Phasellus elit quam, maximus sed ligula vel, egestas placerat dui. Ut sagittis, lectus non iaculis ornare,
-            ipsum ante aliquet diam, maximus maximus magna dolor ut sapien. Proin tincidunt accumsan iaculis. Phasellus
-            pretium, ex eu imperdiet placerat, urna turpis gravida quam, et sodales ligula mauris ac felis. Suspendisse
-            vel lacus eleifend, sollicitudin eros posuere, hendrerit quam. Vivamus sit amet elit vel lorem bibendum
-            molestie. Nullam eleifend congue nisl ac imperdiet. In vel nulla fermentum, dignissim ex in, congue dolor.
-            Maecenas quis malesuada nisl, id facilisis nisl. Pellentesque ante sem, molestie in velit et, porttitor
-            consequat tellus. Nunc blandit, turpis vitae auctor placerat, nulla nisl aliquam ante, a interdum mauris
-            velit in neque.
+        <div className="flex min-h-[calc(calc(100vh)-100px)] items-center justify-center">
+                <div className="max-w-[400px] rounded-lg border border-gray-200 bg-white p-4 shadow-md">
+                        <h1 className="mb-4 text-center text-3xl font-bold">Добро пожаловать в FinancialTracking!</h1>
+                        <p className="mb-6 text-center text-gray-700">
+                                Управляйте своими финансами с легкостью. Контролируйте свои доходы и расходы,
+                                а также составляйте бюджетные планы на различные периоды.
+                        </p>
+                </div>
         </div>
+
     );
 }
