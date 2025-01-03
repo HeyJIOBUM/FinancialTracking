4211801d6e336f9c6b011e7e827d62754d8b9480
HeyJIOBUM <kirill.nichinger@gmail.com>
2025-01-03 11:15:27 +0300

    save Budgets filtering data

--- fintrack-frontend/src/app/me/budgets/page.js
+++ fintrack-frontend/src/app/me/budgets/page.js
@@ -1,10 +1,12 @@
-"use client"
+"use client";
 
+import {useDispatch, useSelector} from 'react-redux';
+import {changeBudgetsFilterData} from "@/redux/slices/filter-data-slice";
 import {useGetBudgetsQuery} from "@/configuration/api/budgets-api";
 import Loading from "@/components/loading";
 import BudgetCard from "@/components/cards/budget-card";
 import {useGetExpensesQuery} from "@/configuration/api/expenses-api";
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {DataViewMode} from "@/types/data-view-mode";
 import DataHeader from "@/components/data-header";
 import {OperationType} from "@/types/operation-type";
@@ -14,14 +16,17 @@ import BudgetEditModal from "@/components/edit-modals/budget-edit-modal";
 import {addMonthsToDate, formatDateToISO, subtractMonthsFromDate} from "@/utils/date-utils";
 
 export default function BudgetsPage() {
+    const dispatch = useDispatch();
+    const filterData = useSelector(state => state.filterDataReducer.value.budgets);
+
     const {data: budgets, error: budgetsError, isLoading: isBudgetsLoading} = useGetBudgetsQuery();
     const {data: expenses, error: expensesError, isLoading: isExpensesLoading} = useGetExpensesQuery();
 
     const dataHeaderInitialState = {
-        chosenCategories: [],
-        fromDate: formatDateToISO(subtractMonthsFromDate(new Date(), 1)),
-        toDate: formatDateToISO(addMonthsToDate(new Date(), 1)),
-        dataViewMode: DataViewMode.LIST
+        chosenCategories: filterData.chosenCategories || [],
+        fromDate: filterData.fromDate || formatDateToISO(subtractMonthsFromDate(new Date(), 1)),
+        toDate: filterData.toDate || formatDateToISO(addMonthsToDate(new Date(), 1)),
+        dataViewMode: filterData.dataViewMode || DataViewMode.LIST,
     };
 
     const [dataHeaderState, setDataHeaderState] = useState(dataHeaderInitialState);
@@ -29,10 +34,10 @@ export default function BudgetsPage() {
 
     const handleModalOpenToggle = () => {
         setIsModalOpen(!isModalOpen);
-    }
+    };
 
     const filterBudgets = (budgets) => {
-        let { chosenCategories, fromDate, toDate } = dataHeaderState;
+        let {chosenCategories, fromDate, toDate} = dataHeaderState;
         fromDate = new Date(fromDate);
         toDate = new Date(toDate);
 
@@ -40,15 +45,11 @@ export default function BudgetsPage() {
             const budgetFromDate = new Date(budget.fromDate);
             const budgetToDate = new Date(budget.toDate);
 
-            const allCategoriesMatch = chosenCategories.length === 0 ||
-                budget.categories.every(category => chosenCategories.includes(category.id));
+            const allCategoriesMatch = chosenCategories.length === 0 || budget.categories.every(category => chosenCategories.includes(category.id));
 
-            return (
-                allCategoriesMatch &&
-                fromDate <= budgetFromDate && budgetFromDate <= toDate
-            );
+            return (allCategoriesMatch && fromDate <= budgetFromDate && budgetFromDate <= toDate);
         });
-    }
+    };
 
     const extractExpensesFromBudgets = (budgets, expenses) => {
         return budgets.map(budget => {
@@ -58,65 +59,57 @@ export default function BudgetsPage() {
 
             const relevantExpenses = expenses.filter(expense => {
                 const expenseDate = new Date(expense.date);
-                return (
-                    budgetCategoriesIds.length === 0 || budgetCategoriesIds.includes(expense.category.id) &&
-                    expenseDate >= fromDate &&
-                    expenseDate <= toDate
-                );
+                return (budgetCategoriesIds.length === 0 || budgetCategoriesIds.includes(expense.category.id) && expenseDate >= fromDate && expenseDate <= toDate);
             });
 
             const totalExpensesAmount = relevantExpenses.reduce((total, expense) => total + expense.amount, 0);
             return {
-                ...budget,
-                totalExpensesAmount,
+                ...budget, totalExpensesAmount,
             };
         });
-    }
+    };
+
+    useEffect(() => {
+        dispatch(changeBudgetsFilterData({
+            chosenCategories: dataHeaderState.chosenCategories,
+            fromDate: dataHeaderState.fromDate,
+            toDate: dataHeaderState.toDate,
+            dataViewMode: dataHeaderState.dataViewMode,
+        }));
+    }, [dataHeaderState, dispatch]);
 
     if (isBudgetsLoading || isExpensesLoading) {
-        return <Loading />;
+        return <Loading/>;
     }
 
     if (budgetsError) {
         throw Error(budgetsError.error);
     }
+
     if (expensesError) {
         throw Error(expensesError.error);
     }
 
-    const filteredBudgets = filterBudgets(budgets)
+    const filteredBudgets = filterBudgets(budgets);
     const enrichedBudgets = extractExpensesFromBudgets(filteredBudgets, expenses);
 
-    return (
-        <div>
+    return (<div>
             <DataHeader
                 categoriesOperationType={OperationType.EXPENSE}
                 dataHeaderState={dataHeaderState}
                 setDataHeaderState={setDataHeaderState}
             />
 
-            {/*Budgets*/}
-            {
-                enrichedBudgets.length > 0 ? (
-                    dataHeaderState.dataViewMode === DataViewMode.LIST ? (
-                        <div className="flex flex-col gap-4 rounded border border-gray-300 p-4">
-                            {
-                                enrichedBudgets.map(enrichedBudget => (
-                                    <BudgetCard key={enrichedBudget.id} enrichedBudget={enrichedBudget} />
-                                ))
-                            }
-                        </div>
-                    ) : (
-                        <BudgetGraphic
-                            enrichedBudgets={enrichedBudgets}
-                        />
-                    )
-                ) : (
+            {/* Budgets */}
+            {enrichedBudgets.length > 0 ? (dataHeaderState.dataViewMode === DataViewMode.LIST ? (
                     <div className="flex flex-col gap-4 rounded border border-gray-300 p-4">
-                        No budgets available.
-                    </div>
-                )
-            }
+                        {enrichedBudgets.map(enrichedBudget => (
+                            <BudgetCard key={enrichedBudget.id} enrichedBudget={enrichedBudget}/>))}
+                    </div>) : (<BudgetGraphic
+                        enrichedBudgets={enrichedBudgets}
+                    />)) : (<div className="flex flex-col gap-4 rounded border border-gray-300 p-4">
+                    No budgets available.
+                </div>)}
 
             <AddElementButton onClick={handleModalOpenToggle}/>
 
@@ -126,6 +119,5 @@ export default function BudgetsPage() {
                 onSave={handleModalOpenToggle}
                 isEditing={false}
             />
-        </div>
-    );
+        </div>);
 }
